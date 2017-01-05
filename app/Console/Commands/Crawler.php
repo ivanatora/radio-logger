523562ce1bfb9b0f9bd1464cc11bee006e2e6e14
Ivan <ivanatora@gmail.com>
2017-01-05 16:03:15 +0200

    crawler going

--- app/Console/Commands/Crawler.php
+++ app/Console/Commands/Crawler.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Channel;
 use App\Recording;
+use LaravelMP3;
 
 class Crawler extends Command
 {
@@ -39,10 +40,55 @@ class Crawler extends Command
      */
     public function handle()
     {
+        $aExtensions = array('mp3');
         $sRootDir = '/home/ivanatora/Downloads/sdrecord-ivanatora/mp3/';
-        
+
         // last saved recording
         $aLastRecording = Recording::orderBy('created_at', 'DESC')->first();
-        print_r($aLastRecording);
+        if (empty($aLastRecording)){
+            $iDiffDays = 999;
+        }
+        else {
+            $sFilename = $aLastRecording->filename;
+            print_r($sFilename);
+            $tsModified = filemtime($sFilename);
+            $tsNow = time();
+            $iDiffDays = ceil(($tsNow - $tsModified) / (24 * 3600));
+        }
+
+        $sCmd = "find $sRootDir -mtime -$iDiffDays";
+        $res = `$sCmd`;
+        $aLines = explode("\n", $res);
+        print_r($aLines);
+
+        foreach ($aLines as $sFullpath){
+            $sExtension = pathinfo($sFullpath, PATHINFO_EXTENSION);
+            if (! in_array($sExtension, $aExtensions)){
+                continue;
+            }
+            $bFileExist = Recording::where('filename', $sFullpath)->count();
+
+            if ($bFileExist){
+                continue;
+            }
+            if (!preg_match('!/(\d{4})_(\d\d)_(\d\d)__(\d\d)_(\d\d)_(\d\d).raw.mp3!', $sFullpath, $aMatches)){
+                continue;
+            }
+
+//            $sDuration = LaravelMP3::getBitrate($sFullpath);
+            $getID3 = new \getID3;
+            $aInfo = $getID3->analyze($sFullpath);
+            $sDuration = (int) $aInfo['playtime_seconds'];
+
+            print "Adding $sFullpath $sDuration\n";
+            $oRecording = new Recording;
+            $oRecording->channel_id = 1;
+            $oRecording->filename = $sFullpath;
+            $oRecording->date_start = $aMatches[1].'-'.$aMatches[2].'-'.$aMatches[3].' '.
+                $aMatches[4].':'.$aMatches[5].':'.$aMatches[6];
+            $oRecording->duration = $sDuration;
+            $oRecording->date_end = date('Y-m-d H:i:s', strtotime('+'.$oRecording->duration.' seconds', strtotime($oRecording->date_start)));
+            $oRecording->save();
+        }
     }
 }
